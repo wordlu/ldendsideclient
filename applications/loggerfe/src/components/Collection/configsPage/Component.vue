a64a4cffa34bfb5238424fc571996953c4532015
wodelu <1058744843@qq.com>
2024-10-31 14:48:18 +0800

    fix: TLZ-186	 【采集配置】设备配置时，前端字段应做必填参数校验

--- applications/loggerfe/src/components/Collection/configsPage/Component.vue
+++ applications/loggerfe/src/components/Collection/configsPage/Component.vue
@@ -170,7 +170,20 @@ const getRemoteFormData = () => {
   }
 };
 
+const validRemoteFormData = () => {
+  if (remoteComponentRef.value && remoteComponentRef.value.getFormData) {
+    return remoteComponentRef.value.validFormData();
+  } else {
+    console.error('远程组件加载错误，无法获取表单数据');
+  }
+}
+
 const onSubmit = async () => {
+  const validFormDataErrMsg = validRemoteFormData()
+  if (validFormDataErrMsg) {
+    ElMessage.error(validFormDataErrMsg)
+    return;
+  }
   getRemoteFormData()
   try {
     const {type, ...deviceparams} = form
