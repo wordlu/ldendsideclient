e300967b6735650d95c86eeb0101bb34c23253fd
wodelu <1058744843@qq.com>
2025-02-20 15:53:37 +0800

    fix: 设备配置禁止传多余值更新后bug修复

--- applications/loggerfe/src/components/Collection/configsPage/Component.vue
+++ applications/loggerfe/src/components/Collection/configsPage/Component.vue
@@ -278,7 +278,9 @@ const getRemoteFormData = () => {
   if (remoteComponentRef.value && remoteComponentRef.value.getFormData) {
     const formData = remoteComponentRef.value.getFormData();
     Object.keys(form).forEach((key) => {
-      delete form[key];
+      if (key !== 'type') {
+        delete form[key];
+      }
     });
     Object.assign(form, formData);
     console.log('远程组件的表单数据:', formData);
