da185f89c09b84adfd798574ec5c91a13d3327c8
wodelu <1058744843@qq.com>
2025-02-26 09:46:09 +0800

    fix: 长城补盲视角不展示od点数

--- applications/loggerfe/src/components/Collection/Indexpage/indexCompare.vue
+++ applications/loggerfe/src/components/Collection/Indexpage/indexCompare.vue
@@ -24,6 +24,7 @@
         <BasicScene 
           ref="basicSceneRef"
           :testDevice="testDevice"
+          :viewportId="viewportId"
           :viewports="viewports"
           :allports="allports"
           :runningDevice="runningDevice"
--- applications/loggerfe/src/components/visualization/index/BasicScene.vue
+++ applications/loggerfe/src/components/visualization/index/BasicScene.vue
@@ -19,7 +19,7 @@
           />
         </el-select>
       </div>
-      <iframe :src="'/pointcloud/realvisualization?allports='+JSON.stringify(allports)+'&portarray='+(testDevice ? portarray1 : '')+'&cloudpointparams='+JSON.stringify(cloudpointparams)" allowfullscreen ameborder="0"></iframe>
+      <iframe :src="'/pointcloud/realvisualization?allports='+JSON.stringify(allports)+'&portarray='+(testDevice ? portarray1 : '')+'&cloudpointparams='+JSON.stringify(cloudpointparams)+'&viewportId='+viewportId" allowfullscreen ameborder="0"></iframe>
       <div class="event-area">
         <iframe :src="getUrl1" height="100%" allowfullscreen ameborder="0"></iframe>
       </div>
@@ -43,7 +43,7 @@
           />
         </el-select>
       </div>
-      <iframe :src="'/pointcloud/realvisualization?allports='+JSON.stringify(allports)+'&portarray='+(testDevice ? portarray2 : '')+'&cloudpointparams='+JSON.stringify(cloudpointparams)" allowfullscreen ameborder="0"></iframe>
+      <iframe :src="'/pointcloud/realvisualization?allports='+JSON.stringify(allports)+'&portarray='+(testDevice ? portarray2 : '')+'&cloudpointparams='+JSON.stringify(cloudpointparams)+'&viewportId='+viewportId" allowfullscreen ameborder="0"></iframe>
       <div class="event-area">
         <iframe :src="getUrl2" height="100%" allowfullscreen ameborder="0"></iframe>
       </div>
@@ -61,7 +61,8 @@ const props = defineProps({
   cloudpointparams: Object,
   viewports: Array,
   runningDevice: Array,
-  testDevice: Boolean
+  testDevice: Boolean,
+  viewportId: String
 });
 
 const options1 = ref([])
