fad940310ef5f65171ec439f774553e84a38195e
wodelu <1058744843@qq.com>
2025-04-18 17:48:58 +0800

    fix：补盲摄像头

--- applications/loggerfe/src/components/visualization/index/BasicSceneBase.vue
+++ applications/loggerfe/src/components/visualization/index/BasicSceneBase.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="canvas-box" @click="canvasClick">
-    <iframe :src="'/pointcloud/realvisualization?showCameras=1&allports='+JSON.stringify(allports)+'&portarray='+getString(currentSelectedSensor)+'&cloudpointparams='+JSON.stringify(cloudpointparams)" width="100%" height="100%" allowfullscreen ameborder="0"></iframe>
+    <iframe :src="'/pointcloud/realvisualization?showBlindCameras=1&allports='+JSON.stringify(allports)+'&portarray='+getString(currentSelectedSensor)+'&cloudpointparams='+JSON.stringify(cloudpointparams)" width="100%" height="100%" allowfullscreen ameborder="0"></iframe>
   </div>
 </template>
  <script setup lang="ts">
